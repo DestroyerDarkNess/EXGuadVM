dbc9b5449a6541f2b40cb8979cecc3910cdc7c05
Destroyer <32405118+DestroyerDarkNess@users.noreply.github.com>
2025-03-12 18:50:24 -0400

    Add EXGuard structure, features, and resource files
    This commit introduces a comprehensive structure for the EXGuard application, including a new Visual Studio solution and project files to organize the codebase. Key features include:
    
    - Implementation of a type inference system and various classes for ILAST transformations.
    - Introduction of runtime protections and obfuscation techniques, enhancing security against reverse engineering.
    - Addition of LZMA encoding and instruction translation components for improved data handling.
    - New resource files for managing application resources and settings, along with a new icon.
    - Updates to the main entry point and method management for better user interaction.
    - Enhanced project configuration and dependency management with NuGet packages.
    
    Overall, these changes significantly improve the functionality, maintainability, and security of the EXGuard application.

---
+++ EXGuardVM/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace EXGuardVM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(632, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Assembly";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.textBox1.Location = new System.Drawing.Point(12, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(614, 13);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 304);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(406, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select any .NET Framework / Core assembly";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Executable Files|*.exe";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(800, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Virtualize Assembly";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Exclude Cctor";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(109, 29);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(131, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Exclude Red Methods";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(246, 29);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(145, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Exclude Unsafe Methods";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(720, 50);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(78, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Select ALL";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(800, 393);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EXGuardVM Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ EXGuardVM/Form1.cs
@@ -0,0 +1,160 @@
+﻿using dnlib.DotNet;
+using EXGuard.Internal;
+using EXGuardVM.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXGuardVM
+{
+    public partial class Form1 : Form
+    {
+        public ModuleDefMD module = null;
+
+        public MethodTreeLoader MethodsLoader = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region " UI "
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string FileName = openFileDialog1.FileName;
+                    if (File.Exists(FileName))
+                    {
+                        ResetUI();
+
+                        if (module != null)
+                        {
+                            module.Dispose();
+                        }
+
+                        if (MethodsLoader != null)
+                        {
+                            GC.SuppressFinalize(MethodsLoader);
+                        }
+
+                        module = ModuleDefMD.Load(FileName);
+
+                        TreeView treeView = new TreeView();
+                        treeView.BackColor = panel1.BackColor;
+                        treeView.ForeColor = panel1.ForeColor;
+                        panel1.Controls.Add(treeView);
+                        treeView.Dock = DockStyle.Fill;
+
+                        MethodsLoader = new MethodTreeLoader(treeView, module);
+                        MethodsLoader.LoadMethods();
+                        label1.Text = module.Assembly.FullName;
+                        textBox1.Text = FileName;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            label1.Text = "Virtualizing Methods...";
+            Task<bool> Protect = ProtectAssembly();
+
+            Protect.GetAwaiter().OnCompleted(() =>
+            {
+                if (Protect.Result)
+                {
+                    label1.Text = "Assembly protected successfully";
+                }
+                else
+                {
+                    label1.Text = "Failed to protect assembly";
+                }
+                button2.Enabled = true;
+            });
+        }
+
+        private void ResetUI()
+        {
+            label1.Text = "";
+            textBox1.Text = "";
+            panel1.Controls.Clear();
+        }
+
+        #endregion
+
+        #region " Protect "
+
+        private async Task<bool> ProtectAssembly()
+        {
+            try
+            {
+                if (module == null) throw new Exception("Assembly not loaded");
+                if (MethodsLoader == null) throw new Exception("MethodsLoader not initialized");
+
+                string output = Path.Combine(Path.GetDirectoryName(module.Location), Path.GetFileNameWithoutExtension(module.Location) + "_VM" + Path.GetExtension(module.Location));
+                string RuntimeVM_Name = "EXGuard.Runtime.dll";
+                List<MethodDef> SelectedMethods = MethodTreeLoader.ResolveMethodsFromTokens(module, MethodsLoader.GetSelectedMethodTokens());
+                HashSet<MethodDef> methodSet = new HashSet<MethodDef>(SelectedMethods);
+                methodSet.Distinct();
+
+                new EXGuardTask().Exceute(module, methodSet, output, RuntimeVM_Name, "", "");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region " MethodsLoader Settings "
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (module != null && MethodsLoader != null)
+                MethodsLoader.All = checkBox4.Checked;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (module != null && MethodsLoader != null)
+                MethodsLoader.ExcludeConstructors = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (module != null && MethodsLoader != null)
+                MethodsLoader.ExcludeRedMethods = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (module != null && MethodsLoader != null)
+                MethodsLoader.ExcludeUnsafeMethods = checkBox3.Checked;
+        }
+
+        #endregion
+
+    }
+}
